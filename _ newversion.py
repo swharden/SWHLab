ea034b26265fee243b0414bedbed6506ffa0350b
Scott W Harden <swharden@gmail.com>
2016-11-20 22:09:05 -0500

    version number only in version.py

--- _ newversion.py
+++ _ newversion.py
@@ -1,14 +1,13 @@
-import time
 if __name__=="__main__":
     version=None
-    for fname in ['setup.py','./swhlab/version.py']:
-        with open(fname) as f:
-            raw=f.read().split("\n")
-            for i,line in enumerate(raw):
-                if line.startswith("__counter__"):
-                    if version is None:
-                        version = int(line.split("=")[1])
-                    raw[i]="__counter__=%d"%(version+1)
-        with open(fname,'w') as f:
-            f.write("\n".join(raw))
-        print("upgraded from version %d to %d"%(version,version+1))
+    fname='./swhlab/version.py'
+    with open(fname) as f:
+        raw=f.read().split("\n")
+        for i,line in enumerate(raw):
+            if line.startswith("__counter__"):
+                if version is None:
+                    version = int(line.split("=")[1])
+                raw[i]="__counter__=%d"%(version+1)
+    with open(fname,'w') as f:
+        f.write("\n".join(raw))
+    print("upgraded from version %03d to %03d"%(version,version+1))
--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 #from distutils.core import setup
 from setuptools import setup
 
-__counter__=53
-__release__=''
-__version__='0.1.%03d'%__counter__+__release__
+from swhlab.version import __version__
 
 setup(
     name='swhlab',
