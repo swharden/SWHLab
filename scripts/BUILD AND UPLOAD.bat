fafa9559244647ed647940592a739bebbcae7d03
Scott W Harden <swharden@gmail.com>
2016-12-01 16:41:38 -0500

    PyPi build 0.1.153

--- scripts/BUILD AND UPLOAD.bat
+++ scripts/BUILD AND UPLOAD.bat
@@ -5,6 +5,7 @@ set /p version=<version.txt
 cd ..
 echo   ### UPLOADING TO PYPI ###
 python setup.py sdist upload
+explorer http://pypi.python.org/pypi/%moduleName%
 echo   ### COMMITTING CHANGES TO GIT ###
 :: using "git" from the command line may require that you add the path to git.exe to your system path:
 :: C:\Users\swharden\AppData\Local\GitHub\PortableGit_d7effa1a4a322478cd29c826b52a0c118ad3db11\cmd\
