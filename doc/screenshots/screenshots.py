2cf783e9fad28729b1937c4dab6eb567cd9b2f91
Scott W Harden <swharden@gmail.com>
2016-12-07 20:32:45 -0500

    new pics

--- doc/screenshots/screenshots.py
+++ doc/screenshots/screenshots.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append("../../../")
+import matplotlib.pyplot as plt
 import swhlab
 plt.close('all')
 fname=r"X:\Data\2P01\2016\2016-09-01 PIR TGOT\16907055.abf"
@@ -12,9 +13,9 @@ for sweep in range(4):
     abf.setsweep(sweep)
     plt.plot(abf.sweepX2,abf.sweepY,alpha=.5)
 plt.ylabel(abf.units2)
-plt.show()
+#plt.show()
 plt.savefig("readme1.jpg")
-
+plt.close('all')
 
 ### DEMO ########################################
 #print(sorted(ap.APs[1].keys()))
@@ -32,9 +33,9 @@ for sweep in range(abf.sweeps):
              '.',label="sweep %d"%sweep,alpha=.5)
 plt.legend(fontsize=6,loc=8)
 plt.ylabel("AP peak value (mV)")
-plt.show()
+#plt.show()
 plt.savefig("readme2.jpg")
-
+plt.close('all')
 
 import swhlab
 import matplotlib.pyplot as plt
@@ -46,5 +47,6 @@ plt.plot(medFreq[:15],'.',ms=10)
 plt.ylabel("Median AP Frequency (Hz)")
 plt.xlabel("sweep number")
 plt.margins(.1,.1)
-plt.show()
+#plt.show()
 plt.savefig("readme3.jpg")
+plt.close('all')
