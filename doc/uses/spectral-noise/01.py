48324115c62c5497d2a447dca943594b6e292a87
Scott W Harden <swharden@gmail.com>
2016-12-12 12:31:16 -0500

    simplify FFT example code

--- doc/uses/spectral-noise/01.py
+++ doc/uses/spectral-noise/01.py
@@ -1,54 +1,37 @@
-"""
-MOST OF THIS CODE IS NOT USED
-ITS COPY/PASTED AND LEFT HERE FOR CONVENIENCE
-"""
+"""Example how to reduce 60 Hz "hum" in electrophysiological recordings"""
 
 import os
 import sys
-
-# in case our module isn't installed (running from this folder)
-if not os.path.abspath('../../../') in sys.path:
+if not os.path.abspath('../../../') in sys.path: # in case our module isn't installed (running from this folder)
     sys.path.append('../../../') # helps spyder get docs
-
 import swhlab
 import matplotlib.pyplot as plt
 import numpy as np
 
 if __name__=="__main__":
-
-    # first let's load our X,Y data
     abfFile=R"X:\Data\DIC1\2013\08-2013\08-16-2013-DP\13816004.abf"
     abf=swhlab.ABF(abfFile) # defaults to sweep 0
-    Xs,Ys=abf.sweepX,abf.sweepY
-
-    # move time domain data into the frequency domain
-    FFT=np.fft.fft(Ys) # these are i/j vectors starting at 0Hz
+    Xs,Ys=abf.sweepX,abf.sweepY # we have X,Y data to work with
     baseFrequency=60 # frequency (Hz) to silence
 
-    for i in range(50): # first 50 harmonics
+    ### convert to frequency domain, silence harmonics, then back to time domain
+    FFT=np.fft.fft(Ys) # frequency data (i/j vectors starting at 0Hz)
+    for i in range(50): # first 50 odd harmonics
         I=int(baseFrequency*i+baseFrequency*len(Ys)/abf.pointsPerSec)
-        FFT[I],FFT[-I]=0+0j,0+0j # silence real and imaginary
+        FFT[I],FFT[-I]=0,0 # remember to silence from both ends of the FFT
+    Ys2=np.fft.ifft(FFT) # all done
 
-
-    # move frequency domain data back into the time domain
-    Ys2=np.fft.ifft(FFT)
-
-    # plot the original / improved trace
+    ### plot the original and new data in X/Y-linked subplots
     plt.figure(figsize=(15,10))
-
     ax1=plt.subplot(211)
     plt.grid()
     plt.title("Original Data")
     plt.plot(Xs,Ys,alpha=.75)
     plt.margins(0,.1)
-
     plt.subplot(212,sharex=ax1,sharey=ax1)
     plt.grid()
     plt.title("Harmonic Suppression")
     plt.plot(Xs,Ys2,alpha=.75)
     plt.margins(0,.1)
-
     plt.tight_layout()
     plt.show()
-
-    print("DONE")
