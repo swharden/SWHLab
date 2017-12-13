1ad9e077e814683da98597f0d048d50869885b85
Scott W Harden <swharden@gmail.com>
2017-12-13 11:54:44 -0500

    0303 first AP time

--- swhlab/analysis/protocols.py
+++ swhlab/analysis/protocols.py
@@ -338,15 +338,29 @@ def proto_0303(theABF):
     ap.detect_time2=8.3
     ap.detect()
     apCount=[]
+    apSweepTimes=[]
 
     for sweepNumber,times in enumerate(ap.get_bySweep("times")):
         apCount.append(len(times))
+        if len(times):
+            apSweepTimes.append(times[0])
+        else:
+            apSweepTimes.append(0)
 
     # plot AP frequency vs time
     plt.figure(figsize=(8,8))
-    plt.plot(np.arange(len(apCount))*abf.sweepLength/60,apCount,'.-')
+
+    ax1=plt.subplot(211)
+    plt.grid(alpha=.4,ls='--')
+    plt.plot(np.arange(len(apCount))*abf.sweepLength/60,apCount,'.-',ms=15)
     comment_lines(abf)
     plt.ylabel("AP Count")
+
+    plt.subplot(212,sharex=ax1)
+    plt.grid(alpha=.4,ls='--')
+    plt.plot(np.arange(len(apCount))*abf.sweepLength/60,apSweepTimes,'.-',ms=15)
+    comment_lines(abf)
+    plt.ylabel("First AP Time (s)")
     plt.xlabel("Experiment Duration (minutes)")
     plt.tight_layout()
     frameAndSave(abf,"IC ramp freq")
